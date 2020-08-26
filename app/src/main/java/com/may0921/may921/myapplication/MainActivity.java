cf0548dfa610074054f65c213ceb473ed63a8a4e
may921 <mayzzv@yahoo.co.jp>
2020-08-27 00:03:31 +0900

    追加

--- app/src/main/java/com/may0921/may921/myapplication/MainActivity.java
+++ app/src/main/java/com/may0921/may921/myapplication/MainActivity.java
@@ -2,13 +2,94 @@ package com.may0921.may921.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long START_TIME = 10000;
+
+    private  TextView mTextViewCountDown;
+    private  Button mButtonStartPause;
+    private  Button getmButtonReset;
+
+
+    private  CountDownTimer mCountDownTimer;
+    private  boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        getmButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            System.out.println(mTimerRunning);
+            if (mTimerRunning) {
+                pauseTimer();
+            }else{
+                startTimer();
+            }
+        }
+        });
+        getmButtonReset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                reset Timer();
+            }
+
+        });
+
+        updateCountDownText();
+
+    }
+    private void startTimer(){
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("スタート");
+                getmButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("一時停止");
+        getmButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer(){
+    }
+
+    private void resetTimer(){
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int)(mTimeLeftInMillis/1000)/60;
+        int seconds = (int)(mTimeLeftInMillis/1000)%60;
+        String timerLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timerLeftFormatted);
+
     }
 }
